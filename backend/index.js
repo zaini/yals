5839991d88cb994453e3df993ade0ce22976f28e
Zaini <alimzaini@outlook.com>
2020-08-16 23:54:33 +0100

    Removed expiration fields.Added UUID for shorturls

--- backend/index.js
+++ backend/index.js
@@ -1,28 +1,30 @@
-const { ApolloServer, gql } = require('apollo-server-express');
-const express = require('express');
-const mongoose = require('mongoose');
+const { ApolloServer, gql } = require("apollo-server-express");
+const express = require("express");
+const mongoose = require("mongoose");
 const resolvers = require("./resolvers");
 const typeDefs = require("./typeDefs");
-require('dotenv').config();
+require("dotenv").config();
 
 const app = express();
 
 // Connecting to MongoDB
 const uri = process.env.MONGODB_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true});
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
 const connection = mongoose.connection;
-connection.once('open', () => {
-    console.log("MongoDB db connection established");
+connection.once("open", () => {
+  console.log("MongoDB db connection established");
 });
 
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
 });
 
 server.applyMiddleware({ app });
 
 const port = process.env.PORT || 4000;
 app.listen(port, () =>
-  console.log(`🚀 Server ready at http://localhost:${port}${server.graphqlPath}`)
-)
+  console.log(
+    `🚀 Server ready at http://localhost:${port}${server.graphqlPath}`
+  )
+);
--- backend/resolvers.js
+++ backend/resolvers.js
@@ -1,25 +1,26 @@
-const { gql } = require('apollo-server-express');
-const Link = require('./models/Link');
+const { gql } = require("apollo-server-express");
+const Link = require("./models/Link");
+const shortid = require('shortid');
+require("dotenv").config();
+
+const domain = process.env.DOMAIN;
 
 // Writing what each function actually returns. This should be from mongoDB
 const resolvers = {
-    Query: {
-        links: () => Link.find(),
-        link: (_, { Short_URL }) =>  Link.find({ Short_URL: Short_URL })
+  Query: {
+    links: () => Link.find(),
+    link: (_, { Short_URL }) => Link.find({ Short_URL: Short_URL }),
+  },
+  Mutation: {
+    // First argument is parent, which we don't need. Second parameter is the arguments, so we destructure for what we want.
+    createLink: (_, { Base_URL }) => {
+      const link = new Link({
+        Base_URL: Base_URL,
+        Short_URL: domain + shortid.generate(),
+      });
+      return link.save();
     },
-    Mutation: {
-        // First argument is parent, which we don't need. Second parameter is the arguments, so we destructure for what we want.
-        createLink: (_, { Base_URL, Short_URL, Expiry_Date }) => {
-            let date = Expiry_Date || new Date();
-            const link = new Link({
-                Base_URL: Base_URL,
-                Short_URL: Short_URL || "testshorturl",
-                Expired: false,
-                Expiry_Date: date.toString()
-            });
-            return link.save();
-        }
-    }
+  },
 };
 
 module.exports = resolvers;
