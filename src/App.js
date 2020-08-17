fc2d5b610fe4d26a5187f70eb5b13c1c7f113f06
Ali Zaini <azaini@uno.edu>
2020-08-16 19:28:42 -0500

    Created basic outline of home page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
-import React from 'react';
+// import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import {Button, Container, Form, FormControl, Nav, Navbar} from 'react-bootstrap';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+  // const [list, setList] = useState(["apple", "oranges"]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <Navbar bg="white" expand="lg">
+            <Navbar.Brand href="/">
+                <img
+                    alt=""
+                    src="logo.jpg"
+                    width="50"
+                    height="41"
+                    className="d-inline-block align-top"
+                />
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="ml-auto">
+                  <Nav.Link href="/contact">Contact</Nav.Link>
+                  <Nav.Link href="/login">Login</Nav.Link>
+                  <Nav.Link id="signup" href="/signUp">Sign Up</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+        <hr></hr>
+        <div>
+          <form class="wrapper">
+            <input type="text" id="link" placeholder="Make your links shorter"></input>
+            <input type="submit" id="link"value="Convert!"></input>
+          </form> 
+          <div class='loader'>
+            <div class='loader--dot'></div>
+            <div class='loader--dot'></div>
+            <div class='loader--dot'></div>
+            <div class='loader--dot'></div>
+            <div class='loader--dot'></div>
+            <div class='loader--dot'></div>
+            <div class='loader--text'></div>
+          </div>
+        </div>
     </div>
   );
 }
 
 export default App;
+  //   <div id="main">
+  //     <h1>fruits</h1>
+  //     <p>fruit list</p>
+  //     <ul id="fruit-list">
+  //     {
+  //       list.map(fruit => {
+  //         return <li>{fruit}</li>
+  //       })
+  //     }
+  //     <button onClick={()=>setList([list, "random fruit"])}>add fruit</button>
+  //     </ul>
+  //   </div>
+  // );
