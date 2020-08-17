fd16654d43a3136dd3aba477aa5dd2c035ed3a79
Zaini <alimzaini@outlook.com>
2020-08-17 01:20:16 +0100

    createLink mutation for duplicate links. + queries

--- backend/resolvers.js
+++ backend/resolvers.js
@@ -1,6 +1,6 @@
 const { gql } = require("apollo-server-express");
 const Link = require("./models/Link");
-const shortid = require('shortid');
+const shortid = require("shortid");
 require("dotenv").config();
 
 const domain = process.env.DOMAIN;
@@ -9,16 +9,27 @@ const domain = process.env.DOMAIN;
 const resolvers = {
   Query: {
     links: () => Link.find(),
-    link: (_, { Short_URL }) => Link.find({ Short_URL: Short_URL }),
+    link_by_short_url: (_, { Short_URL }) => Link.find({ Short_URL: Short_URL }),
+    link_by_base_url: (_, { Base_URL }) => Link.find({ Base_URL: Base_URL }),
   },
   Mutation: {
     // First argument is parent, which we don't need. Second parameter is the arguments, so we destructure for what we want.
     createLink: (_, { Base_URL }) => {
-      const link = new Link({
-        Base_URL: Base_URL,
-        Short_URL: domain + shortid.generate(),
+      Link.findOne({ Base_URL: Base_URL }, (err, res) => {
+        if (res == null) {
+          // If a Link doesn't exists for the given URL, create and return a Link
+          console.log(`${Base_URL} does not exist. Creating and returning a new Link.`);
+          const link = new Link({
+            Base_URL: Base_URL,
+            Short_URL: domain + shortid.generate(),
+          });
+          return link.save();
+        } else {
+          // If a Link already exists for the given URL, return that Link
+          console.log(`${Base_URL} does exist. Returning ${res}.`);
+          return res;
+        }
       });
-      return link.save();
     },
   },
 };
