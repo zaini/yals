91e9b28317785f1bd39e5a4be4266ed6c2018219
Zaini <alimzaini@outlook.com>
2020-08-18 01:07:52 +0100

    Added enter key handling for link input.

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,6 +1,10 @@
 import React, { useState, Component } from "react";
 import { Button, Container } from "react-bootstrap";
-const { createApolloFetch } = require('apollo-fetch');
+const { createApolloFetch } = require("apollo-fetch");
+
+const fetch = createApolloFetch({
+  uri: "http://localhost:4000/graphql",
+});
 
 export default class HomePage extends Component {
   constructor(props) {
@@ -12,23 +16,19 @@ export default class HomePage extends Component {
   }
 
   shorten() {
-    const fetch = createApolloFetch({
-        uri: 'http://localhost:4000/graphql',
-      });
-
     console.log(this.state.link);
 
     fetch({
-        query: `{
+      query: `{
             link_by_base_url(Base_URL: "${this.state.link}"){
               id
               Short_URL
               Base_URL
             }
-          }`
-    }).then(res => {
-        console.log(res)
-    })
+          }`,
+    }).then((res) => {
+      console.log(res.data.link_by_base_url[0]);
+    });
   }
 
   render() {
@@ -38,7 +38,10 @@ export default class HomePage extends Component {
           id="link"
           placeholder="Make your links shorter"
           value={this.state.link}
-          onChange={(e) => this.setState({ link: e.target.value })}
+          onKeyDown={(e) => {if(e.key === 'Enter'){this.shorten()}}}
+          onChange={(e) => {
+            this.setState({ link: e.target.value });
+          }}
         ></input>
         <br />
         <Button id="submit-button" onClick={() => this.shorten()}>
