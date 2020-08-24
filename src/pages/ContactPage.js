ea2e5280f007ef6e52c82c5f030f40be412633e0
Ali Zaini <azaini@uno.edu>
2020-08-24 12:30:12 -0500

    Started Contact Page

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -11,7 +11,23 @@ export default class ContactPage extends Component {
   render() {
     return (
       <Box>
-        This is a ContactPage
+        <div id= "contact-form">
+          <div>
+            <input type = "text" id = "name" placeholder = "Name" ></input>
+          </div>
+          <div>
+            <input type = "email" id = "email" placeholder = "Email" ></input>
+          </div>
+          <div>
+            <input type = "text" id = "subject" placeholder = "Subject" ></input>
+          </div>
+          <div>
+            <textarea rows = "4" id = "message" placeholder = "Message" ></textarea>
+          </div>
+          <button type = "submit" > Submit </button>
+        </div>
+        <script src="../form/form-model.js"></script>
+        <script src="../form/form-controller.js"></script>
       </Box>
     );
   }
