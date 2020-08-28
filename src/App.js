32681c26903788a85af43a8ad34a76e795edb39e
Zaini <alimzaini@outlook.com>
2020-08-28 16:05:52 +0100

    Logging in now changes navbar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,61 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import HomePage from "./pages/HomePage.js";
 import RedirectPage from "./pages/RedirectPage.js";
 import ContactPage from "./pages/ContactPage";
 import LoginPage from "./pages/LoginPage";
 import SignUpPage from "./pages/SignUpPage";
-import { Box, Flex, Text, Image, Link, Button, useColorMode, Icon } from "@chakra-ui/core";
+import {
+  Box,
+  Flex,
+  Text,
+  Image,
+  Link,
+  Button,
+  useColorMode,
+  Icon,
+} from "@chakra-ui/core";
+import { useQuery } from "urql";
+
+const ME_QUERY = `query {
+  me {
+    id
+    UserName
+  }
+}`;
 
 function App() {
+  const [res, reexecuteQuery] = useQuery({ query: ME_QUERY });
+  const { data, fetching, error } = res;
+
   const { colorMode, toggleColorMode } = useColorMode();
+
+  let body = null;
+
+  console.log(data);
+  if (fetching) {
+  } else if (data.me !== null) {
+    body = (
+      <>
+        <Link className="link" href={"/account"}>
+          {data.me.UserName}
+        </Link>
+        <Button>Logout</Button>
+      </>
+    );
+  } else {
+    body = (
+      <>
+        <Link className="link" href={"/login"}>
+          Login
+        </Link>
+        <Link className="link" href={"/signup"}>
+          Sign Up
+        </Link>
+      </>
+    );
+  }
 
   return (
     <Box>
@@ -25,13 +71,21 @@ function App() {
           />
           <Text pl={3}>Link-Shortener</Text>
         </Flex>
+
         <Box className="navbar-links">
-          <Link className="link" href={"/"}>Home</Link>
-          <Link className="link" href={"/contact"}>Contact</Link>
-          <Link className="link" href={"/login"}>Login</Link>
-          <Link className="link" href={"/signup"}>Sign Up</Link>
+          <Link className="link" href={"/"}>
+            Home
+          </Link>
+          <Link className="link" href={"/contact"}>
+            Contact
+          </Link>
+          {body}
           <Button onClick={toggleColorMode} marginRight="50px">
-            {colorMode === "light" ? <Icon name="moon" width="auto"/> : <Icon name="sun" />}
+            {colorMode === "light" ? (
+              <Icon name="moon" width="auto" />
+            ) : (
+              <Icon name="sun" />
+            )}
           </Button>
         </Box>
       </Flex>
