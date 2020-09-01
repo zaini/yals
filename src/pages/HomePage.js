de7f4f8e1993ff37b40e8591dc9b037a79b88fa1
Zaini <alimzaini@outlook.com>
2020-09-01 02:52:25 +0100

    Changed homepage to functional comp using form

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,88 +1,104 @@
-import React, { Component, useState } from "react";
-import { Box, Button, Input, Icon } from "@chakra-ui/core";
+import React, { useState } from "react";
+import {
+  Box,
+  Button,
+  Input,
+  Icon,
+  FormControl,
+  FormErrorMessage,
+} from "@chakra-ui/core";
 import { createApolloFetch } from "apollo-fetch";
-import { CopyToClipboard } from 'react-copy-to-clipboard';
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import { useForm } from "react-hook-form";
 import QRCode from "qrcode.react";
 
 const domain = "azaini.me/";
 const fetch = createApolloFetch({ uri: "http://localhost:4000/graphql" });
 
-export default class HomePage extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      link: "",
-      short_link: undefined,
-      copied: false,
-    };
-  }
+const HomePage = () => {
+  const [short_link, setShort_Link] = useState(undefined);
+  const [copied, setCopied] = useState(false);
+  const { register, handleSubmit, errors, setError } = useForm();
 
-  shorten() {
-    fetch({
-      query: `{link_by_base_url(Base_URL: "${this.state.link}"){
+  const onSubmit = async ({ link }) => {
+    console.log(`Form Data: ${link}`);
+    let response = await shorten(link);
+    console.log("Shorten response: " + response);
+    setShort_Link(response);
+  };
+
+  const shorten = async (link) => {
+    return await fetch({
+      query: `{link_by_base_url(Base_URL: "${link}"){
         Short_URL
       }
     }`,
     }).then((res) => {
-      console.log(res.data.link_by_base_url);
+      // console.log(res.data.link_by_base_url);
       if (res.data.link_by_base_url[0]) {
-        console.log("Found existing link");
-        this.setState({ short_link: res.data.link_by_base_url[0].Short_URL });
+        // console.log("Found existing link");
+        return res.data.link_by_base_url[0].Short_URL;
       } else {
-        console.log("Did not find an existing link. Creating new one.");
-        fetch({
+        // console.log("Did not find an existing link. Creating new one.");
+        return fetch({
           query: `mutation{
-            createLink(Base_URL: "${this.state.link}"){
+            createLink(Base_URL: "${link}"){
               Short_URL
             }
           }`,
         }).then((res) => {
           if (res) {
-            this.setState({ short_link: res.data.createLink.Short_URL });
+            return res.data.createLink.Short_URL;
           } else {
-            console.log("Did not create a link successfully");
+            setError("link", {
+              type: "manual",
+              message: "Could not create a link successfully.",
+            });
           }
         });
       }
     });
-  }
+  };
 
-  render() {
-    return (
-      <Box id="homepage">
-        <Input
-          placeholder="Make your links shorter"
-          value={this.state.link}
-          onKeyDown={(e) => {
-            if (e.key === "Enter") {
-              this.shorten();
-            }
-          }}
-          onChange={(e) => {
-            this.setState({ link: e.target.value });
-          }}
-        ></Input>
-        <br />
+  return (
+    <Box id="homepage">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <FormControl isInvalid={errors.link} mt={4}>
+          <Input
+            type="text"
+            placeholder="Make your links shorter"
+            name="link"
+            ref={register}
+          />
+          <FormErrorMessage>
+            {errors.link && errors.link.message}
+          </FormErrorMessage>
+        </FormControl>
 
-        <Button id="submit-button" onClick={() => this.shorten()}>
+        <Button mt={4} mb={4} type="submit">
           Convert!
         </Button>
-        <br />
+      </form>
 
-        <Box id="result">
-          {this.state.short_link !== undefined
-            ? [
-              domain + this.state.short_link,
-              <CopyToClipboard id="copyButton" text={domain + this.state.short_link} onCopy={() => this.setState({ copied: true })}>
+      <Box id="result">
+        {short_link !== undefined
+          ? [
+              domain + short_link,
+              <CopyToClipboard
+                id="copyButton"
+                text={domain + short_link}
+                onCopy={() => setCopied(true)}
+              >
                 <button>
-                  {this.state.copied ? <Icon name="check" /> : <Icon name="copy" />}
+                  {copied ? <Icon name="check" /> : <Icon name="copy" />}
                 </button>
               </CopyToClipboard>,
-              <QRCode value={domain + this.state.short_link} />,
+              <QRCode value={domain + short_link} />,
             ]
-            : null}
-        </Box>
+          : null}
       </Box>
-    );
-  }
-}
+    </Box>
+  );
+};
+
+export default HomePage;
